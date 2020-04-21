5a24122d920dce2d7aec1b8c04226ca945213b73
Alexander Schneider <alexander@schneider.gg>
2020-04-21 14:15:05 +0200

    Add conversion to datomic for demonstration purposes

--- src/dialog_core/data_model.clj
+++ src/dialog_core/data_model.clj
@@ -1,31 +1,37 @@
 (ns dialog-core.data-model
   (:require [hodur-engine.core :as hodur]
-            [hodur-spec-schema.core :as hodur-spec]))
+            [hodur-spec-schema.core :as hodur-spec]
+            [hodur-datomic-schema.core :as hodur-datomic]))
 
 (def core-data (hodur/init-schema
-                 '[^{:spec/tag-recursive true}
+                 '[^{:spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    Statement
                    [^Author author
                     ^String content
                     ^DateTime created
                     ^ID id]
 
-                   ^{:spec/tag-recursive true}
+                   ^{:spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    Author
                    [^String nickname
                     ^ID id]
 
                    ^{:union true
-                     :spec/tag-recursive true}
+                     :spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    EdgeTarget
                    [Statement Edge]
 
                    ^{:enum true
-                     :spec/tag-recursive true}
+                     :spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    EdgeType
                    [SUPPORT ATTACK UNDERCUT]
 
-                   ^{:spec/tag-recursive true}
+                   ^{:spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    Edge
                    [^Author author
                     ^Statement source
@@ -37,7 +43,8 @@
                       :default 1} version
                     ^ID id]
 
-                   ^{:spec/tag-recursive true}
+                   ^{:spec/tag-recursive true
+                     :datomic/tag-recursive true}
                    Discussion
                    [^String title
                     ^String description
@@ -48,11 +55,13 @@
 ;; Hauptsächliche Fragen:
 ;; * Geht man den DBAS Weg mit Topics zu denen statements und argumente
 ;;   eindeutig zugewiesen sind?
-;  * Geht man den EDEN weg in denen Statements und Argumente frei sind
+;  * Geht man den EDEN weg in dem Statements und Argumente frei sind
 ;;   (siehe oben) Dann ist eine Diskussion nur eine Fragestellung mit
 ;;   argumenten die am Root hängen
 ;; * Braucht in dieser Art zu denken jedes objekt eine ID?
 
 
 (def spec-schema (hodur-spec/schema core-data))
-spec-schema
+(comment spec-schema)
+(def datomic-schema (hodur-datomic/schema core-data))
+(comment datomic-schema)
