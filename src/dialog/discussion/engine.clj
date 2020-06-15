06438a5cdb6f7d508db5123fcfe191e876a75a6b
Alexander Schneider <alexander@schneider.gg>
2020-06-15 17:07:53 +0200

    Add more crud

--- src/dialog/discussion/engine.clj
+++ src/dialog/discussion/engine.clj
@@ -1,6 +1,9 @@
 (ns dialog.discussion.engine)
 
-(def database (atom {}))
+(def database (atom {:closed-discussions #{}}))
+;; :closed-discussions must be a set that is present. Otherwise the close and
+;; open discussions functions behave improperly. Wont matter once, we use a real
+;; database
 
 (defn create-discussion
   "Returns a newly created discussion map. Checks for duplicates"
@@ -19,3 +22,22 @@
     (when-not (some #(= (:id argument) %) starting-arguments-ids)
       (update discussion :starting-arguments conj argument))))
 
+(defn save-discussion!
+  "Saves discussion into the database."
+  [discussion]
+  (swap! database assoc-in [:discussions (:id discussion)] discussion))
+
+(defn delete-discussion!
+  "Deletes the discussion from the database."
+  [discussion]
+  (swap! database update :discussions dissoc (:id discussion)))
+
+(defn close-discussion!
+  "Closes a discussion. Users can not discuss in this issue anymore."
+  [discussion]
+  (swap! database update :closed-discussions conj (:id discussion)))
+
+(defn open-discussion!
+  "Opens a closed discussion. Does not check whether the discussion is closed."
+  [discussion]
+  (swap! database update :closed-discussions disj (:id discussion)))
