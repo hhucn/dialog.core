6549cc0c92b9df5cf90725036ab0a81416024a2b
Alexander Schneider <alexander@schneider.gg>
2020-06-16 16:18:18 +0200

    Change to correct schema for datomic db

--- src/dialog/discussion/administration.clj
+++ src/dialog/discussion/administration.clj
@@ -11,37 +11,43 @@
   "Returns a newly created discussion map."
   [title description & opts]
   (merge
-    {:created (LocalDateTime/now)
-     :modified (LocalDateTime/now)
-     :id (UUID/randomUUID)
-     :closed false
-     :starting-arguments []}
+    {:discussion/created (LocalDateTime/now)
+     :discussion/modified (LocalDateTime/now)
+     :discussion/id (UUID/randomUUID)
+     :discussion/closed false
+     :discussion/starting-arguments []}
     opts
-    {:title title
-     :description description}))
+    {:discussion/title title
+     :discussion/description description}))
 
+;; TODO this has to be rewritten to work with a real db
 (defn add-starting-argument
   "Adds a starting argument and returns the modified discussion. Checks for duplicates."
   [discussion argument]
-  (let [starting-arguments-ids (map :id (:starting-arguments discussion))]
-    (when-not (some #(= (:id argument) %) starting-arguments-ids)
-      (update discussion :starting-arguments conj argument))))
+  (let [starting-arguments-ids
+        (map :discussion/id (:discussion/starting-arguments discussion))]
+    (when-not (some #(= (:discussion/id argument) %) starting-arguments-ids)
+      (update discussion :discussion/starting-arguments conj argument))))
 
+;; TODO this has to be rewritten to work with a real db
 (defn- save-discussion!
   "Saves discussion into the database."
   [discussion]
   (swap! database assoc-in [:discussions (:id discussion)] discussion))
 
+;; TODO this has to be rewritten to work with a real db
 (defn delete-discussion!
   "Deletes the discussion from the database."
   [discussion]
   (swap! database update :discussions dissoc (:id discussion)))
 
+;; TODO this has to be rewritten to work with a real db
 (defn close-discussion!
   "Closes a discussion. Users can not discuss in this issue anymore."
   [discussion]
   (swap! database update :closed-discussions conj (:id discussion)))
 
+;; TODO this has to be rewritten to work with a real db
 (defn open-discussion!
   "Opens a closed discussion. Does not check whether the discussion is closed."
   [discussion]
