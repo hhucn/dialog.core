f0760d6312e2131a7e72b3ef6402c0e9c0e91fa4
Alexander Schneider <alexander@schneider.gg>
2020-06-17 15:33:52 +0200

    Remove hodur and use selfmade specs when needed

--- src/dialog/core/models.clj
+++ src/dialog/core/models.clj
@@ -1,7 +1,5 @@
 (ns dialog.core.models
-  (:require [hodur-engine.core :as hodur]
-            [hodur-spec-schema.core :as hodur-spec]
-            [hodur-datomic-schema.core :as hodur-datomic]))
+  (:require [clojure.spec.alpha :as s]))
 
 (def datomic-schema
   [;; Statement
@@ -69,67 +67,16 @@
     :db/cardinality :db.cardinality/many
     :db/doc "The arguments at the source of the discussion-graph"}])
 
-(def core-data
-  (hodur/init-schema
-    '[^{:spec/tag true
-        :datomic/tag true}
-      default
+;; Specs. Please keep in sync with model above. Extend as needed.
+(s/def :discussion/title string?)
+(s/def :discussion/description string?)
+(s/def :discussion/states
+  (s/coll-of #{:discussion.state/open :discussion.state/closed :discussion.state/private}
+             :distinct true))
+(s/def :discussion/starting-arguments (s/coll-of map?))     ;; Specify more
+(s/def ::discussion (s/keys :req-un [:discussion/title :discussion/description :discussion/states
+                                     :discussion/starting-arguments]))
 
-      Statement
-      [^Author author
-       ^String content
-       ^{:type Integer
-         :default 1} version
-       ^DateTime created
-       ^DateTime modified]
-
-      Author
-      [^String nickname
-       ^DateTime created
-       ^DateTime modified]
-
-      ^{:union true}
-      ArgumentTarget
-      [Statement Argument]
-
-      ^{:enum true}
-      ArgumentType
-      [support attack undercut]
-
-      Argument
-      [^Author author
-       ^{:type Statement
-         :cardinality [0 n]} premises
-       ^ArgumentType type
-       ^ArgumentTarget conclusion
-       ^DateTime created
-       ^DateTime modified
-       ^{:type Integer
-         :default 1} version]
-
-      ^{:enum true}
-      State
-      [closed private open]
-
-      Discussion
-      [^String title
-       ^String description
-       ^DateTime created
-       ^DateTime modified
-       ^{:type Argument
-         :cardinality [0 n]} starting-arguments
-       ^{:type State
-         :cardinality [0 n]} state]]))
-
-(def spec-schema (hodur-spec/schema core-data))
-;; This defspecs needs to be run to have the specs in this namespace.
-(hodur-spec/defspecs core-data)
-(comment spec-schema)
-;; The following will define all specs with the prefix foo instead of this namespace.
-;; We can use this in tests to generate correct specs. There seems to be no option to cut the namespace
-;; alltogether. one could annotate aliases for every attribute, but that is as tedious as writing
-;; separate specs and datomic.
-(comment (hodur-spec/defspecs core-data {:prefix :foo}))
-
-(def datomic-schema (hodur-datomic/schema core-data))
-(comment datomic-schema)
+(s/explain ::discussion {:title "bärchen" :description "pärchen"
+                         :states [:discussion.state/open :discussion.state/private]
+                         :starting-arguments []})
