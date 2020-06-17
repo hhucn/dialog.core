e058f7a3e11891b7f83f7bc08a558453349711f7
Alexander Schneider <alexander@schneider.gg>
2020-06-17 15:43:59 +0200

    Change config to read from env

--- src/dialog/discussion/config.clj
+++ src/dialog/discussion/config.clj
@@ -3,10 +3,12 @@
 ;; Dev config. Need a proper way to handle switch when in production.
 ;; ##################################################################
 (def datomic
-  {:server-type :peer-server
-   :access-key "secretaccess"
-   :secret "secretsecret"
-   :endpoint "localhost:8998"
-   :validate-hostnames false})
+  (let [host (or (System/getenv "DATOMIC_HOST") "localhost")
+        port (or (System/getenv "DATOMIC_PORT") "8998")]
+    {:server-type :peer-server
+     :access-key (or (System/getenv "DATOMIC_ACCESS_KEY") "secretaccess")
+     :secret (or (System/getenv "DATOMIC_SECRET") "secretsecret")
+     :endpoint (str host ":" port)
+     :validate-hostnames false}))
 
-(def db-name "dev-db")
+(def db-name (or (System/getenv "DATOMIC_DISCUSSION_DB_NAME") "dev-db"))
