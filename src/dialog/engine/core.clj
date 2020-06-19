51ff272f6c6a13ed41e526b706639a50ce8dbf56
Christian Meter <cmeter@googlemail.com>
2020-06-19 12:36:13 +0200

    Start with react multimethod
    Co-authored-by: Alexander Schneider <alexander.schneider@uni-duesseldorf.de>

--- src/dialog/engine/core.clj
+++ src/dialog/engine/core.clj
@@ -1,4 +1,5 @@
-(ns dialog.engine.core)
+(ns dialog.engine.core
+  (:require [dialog.discussion.database :as database]))
 
 (def ... :todo)
 
@@ -39,8 +40,42 @@
 
 ;; -----------------------------------------------------------------------------
 
+(defn find-attacking-argument [attitude arguments]
+  ;; choose an attacking argument
+  {})
+
+(defn find-arguments-for-opinion [attitude arguments]
+  {})
+
 ;; Das was der user auswählt
 (defmulti react (fn [current-step reaction] reaction))
+
+(defmethod react :arguments/present
+  [_step {:keys [discussion/title is-start? user/attitude] :as args}]
+  (let [arguments (if is-start?
+                    (database/starting-arguments-by-title title)
+                    (find-arguments-for-opinion attitude (database/arguments-by-title title)))]
+    [[:arguments/present (merge args {:arguments/present arguments})]]))
+
+(defmethod react :argument/chosen
+  [_step args]
+  ;; User has chosen an argument and the system is now attacking for it. This step
+  ;; chooses the attacking argument.
+  (let [attacking-argument (find-attacking-argument (:user/attitude args) (:argument/chosen args))]
+    [[:reactions/present (merge args {:argument/attacking attacking-argument})]]))
+
+(defmethod react :reaction/support
+  [_step args]
+  ;; User has chosen that they support the presented argument. Now, the system
+  ;; looks for a new attacking argument.
+  (let [attacking-argument (find-attacking-argument :pro (:argument/chosen args))]
+    [[:reactions/present (merge args {:argument/attacking attacking-argument})]]))
+
+(defmethod react :reaction/defend
+  [_step args])
+;; User accepts the presented argument, BUT still wants to defend their own
+;; opinion.
+
 
 (defmethod react :arguments/pro
   [_current-step argument]
