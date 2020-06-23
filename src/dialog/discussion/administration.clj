12b8d519f6c011a2e5d79b20ac3e07f2a8486363
Alexander Schneider <alexander@schneider.gg>
2020-06-23 15:57:59 +0200

    Rename `open-discussion!` to `reopen-discussion!`

--- src/dialog/discussion/administration.clj
+++ src/dialog/discussion/administration.clj
@@ -1,5 +1,6 @@
 (ns dialog.discussion.administration
   (:require [clojure.spec.alpha :as s]
+            [dialog.discussion.database :as database]
             [dialog.discussion.models :as models]))
 
 (defn empty-discussion
@@ -17,3 +18,14 @@
 (s/fdef empty-discussion
         :args (s/cat :title string? :description string?)
         :ret ::models/discussion)
+
+(defn open-discussion!
+  "Create a new discussion in the database. Returns the discussion if successful,
+  or nil when the title is already reserved."
+  [title description]
+  (try
+    (let [new-discussion (empty-discussion title description)]
+      (database/save-discussion! new-discussion)
+      new-discussion)
+    (catch Exception _e
+      nil)))
