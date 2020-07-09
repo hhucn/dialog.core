00ce0678d7cac46b3b5ce913d330a3f1b59aaf8c
Christian Meter <cmeter@googlemail.com>
2020-07-09 21:40:25 +0200

    Extract fn to display current discussions

--- src/dialog/engine/cli.clj
+++ src/dialog/engine/cli.clj
@@ -3,15 +3,18 @@
             [dialog.engine.core :as engine]
             [clojure.string :as string]))
 
+(defn- list-discussion [discussions]
+  (string/join
+    "\n"
+    (map-indexed
+      (fn [idx [_id title]] (format "[%s] %s\n" idx title))
+      discussions)))
+
 (defn start []
   (let [discussions (database/all-discussion-titles-and-ids)]
     (println
-      (format "Welcome 🥳! Choose a discussion:\n")
-      (string/join
-        "\n"
-        (map-indexed
-          (fn [idx [_id title]] (format "[%s] %s\n" idx title))
-          discussions)))
+      "Welcome 🥳! Choose a discussion:\n"
+      (list-discussion discussions))
     (let [index (Integer/parseInt (read-line))
           [id title] (nth discussions index)]
       {:discussion/id id
