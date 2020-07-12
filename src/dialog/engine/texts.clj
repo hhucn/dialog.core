f97a74adf918b4d32b2467b0373bd49f666b7f5d
Christian Meter <cmeter@googlemail.com>
2020-07-12 18:45:10 +0200

    Add ns for string generation

---
+++ src/dialog/engine/texts.clj
@@ -0,0 +1,34 @@
+(ns dialog.engine.texts
+  "Defines strings, which can be used for text-representation."
+  (:require [clojure.string :as string]
+            [clojure.spec.alpha :as s]
+            [dialog.discussion.models :as models]))
+
+(defn argument
+  "Format an argument. Example:
+  \"Kangaroo
+  We should get a dog, because: dogs can act as watchdogs.\""
+  []
+  "%s\n%s, because: %s.")
+
+(defn concat-premises
+  "Takes a collection of premises and concatenates them."
+  [premises]
+  (string/join " and " (map :statement/content premises)))
+
+(s/fdef concat-premises
+        :args (s/cat :premises (s/coll-of ::models/statement))
+        :ret string?)
+
+(defn avatar-with-nickname
+  "Pick a random avatar based on the user's nickname. Returns a string, where
+  the avatar is prepended to the nickname."
+  [nickname]
+  (let [avatars ["👩‍🦰" "👨‍🦰" "🎃" "👩‍🦳" "🧔" "🧑" "👨‍🌾" "👩‍🌾" "👨🏿‍🎤" "👩‍🏫" "👨‍💻" "👩‍🎤"
+                 "👸" "👾" "🙄" "😬" "🤢" "😈" "👻" "🤓" "🤪"]
+        picked (nth avatars (mod (hash nickname) (count avatars)))]
+    (format "%s %s" picked nickname)))
+
+(s/fdef avatar-with-nickname
+        :args string?
+        :ret string?)
