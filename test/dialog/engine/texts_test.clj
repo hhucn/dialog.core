eca8d635359c79e07261ac76556f57459233fe24
Christian Meter <cmeter@googlemail.com>
2020-07-13 13:25:52 +0200

    Add test for avatar with nickname

--- test/dialog/engine/texts_test.clj
+++ test/dialog/engine/texts_test.clj
@@ -5,10 +5,16 @@
     [dialog.engine.texts :as texts]
     [dialog.test.utilities :as tutils]))
 
-(deftest argument-test
+(deftest argument-with-author-test
   (testing "Function should always return a format string."
-    (is (string/includes? (texts/argument) "%s"))))
+    (is (string/includes? (texts/argument-with-author) "%s"))))
 
 (deftest concat-premises-test
   (testing "Generative tests when concatenating premises."
     (is (tutils/check? `texts/concat-premises))))
+
+(deftest avatar-with-nickname-test
+  (testing "Nickname should still remain in the resulting string."
+    (is (tutils/check? `texts/avatar-with-nickname))
+    (let [nickname "kangaroo"]
+      (is (string/includes? (texts/avatar-with-nickname nickname) nickname)))))
