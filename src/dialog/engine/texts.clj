8017271ed3cbcef223a80347ccb7f9118b354a65
Christian Meter <cmeter@googlemail.com>
2020-07-13 22:37:10 +0200

    Add more texts for reactions

--- src/dialog/engine/texts.clj
+++ src/dialog/engine/texts.clj
@@ -8,8 +8,11 @@
   "Format an argument. Example:
   \"Kangaroo
   We should get a dog, because: dogs can act as watchdogs.\""
-  []
-  "%s\n\t%s, because: %s.")
+  [argument-type]
+  (case argument-type
+    :argument.type/attack "%s\n\t%s is not a good idea, because: %s."
+    :argument.type/undercut "TODO Undercuts are not yet implemented"
+    "%s\n\t%s, because: %s."))
 
 (defn statement-with-author
   "Format a statement. Example:
@@ -17,6 +20,17 @@
   We should get a dog\""
   []
   "%s\n\t%s")
+
+(defn reactions
+  "Get texts for possible reactions."
+  [reaction]
+  (get
+    {:reaction/undermine "Attack the premise(s) (undermine)"
+     :reaction/rebut "Attack the conclusion (rebut)"
+     :reaction/undercut "Attack the relation (undercut)"
+     :reaction/support "Support the conclusion with own premises (use argument's premises as conclusion and add own premises)"
+     :reaction/defend "Defend my own point of view (add own premises to conclusion)"}
+    reaction))
 
 (defn concat-premises
   "Takes a collection of premises and concatenates them."
