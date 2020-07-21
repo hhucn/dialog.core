0f140757f1e5f9b126cc371aa25f8bad3f717c71
Christian Meter <cmeter@googlemail.com>
2020-07-21 17:03:54 +0200

    Use dev-local in config

--- src/dialog/discussion/config.clj
+++ src/dialog/discussion/config.clj
@@ -1,14 +1,11 @@
-(ns dialog.discussion.config)
+(ns dialog.discussion.config
+  (:require [dialog.utils :as utils]))
 
 ;; Dev config. Need a proper way to handle switch when in production.
 ;; ##################################################################
 (def datomic
-  (let [host (or (System/getenv "DATOMIC_HOST") "localhost")
-        port (or (System/getenv "DATOMIC_PORT") "8998")]
-    {:server-type :peer-server
-     :access-key (or (System/getenv "DATOMIC_ACCESS_KEY") "secretaccess")
-     :secret (or (System/getenv "DATOMIC_SECRET") "secretsecret")
-     :endpoint (str host ":" port)
-     :validate-hostnames false}))
+  {:system "development"
+   :server-type :dev-local
+   :storage-dir (utils/create-storage-directory!)})
 
 (def db-name (or (System/getenv "DATOMIC_DISCUSSION_DB_NAME") "dev-db"))
