bc851b84921939ba96c0f236a8eb4ae2194c855a
Christian Meter <cmeter@googlemail.com>
2020-07-21 10:44:02 +0200

    Add tests for ident-map->value

--- test/dialog/utils_test.clj
+++ test/dialog/utils_test.clj
@@ -1,8 +1,9 @@
 (ns dialog.utils-test
   (:require [clojure.test :refer :all]
-            [dialog.utils :as utils]))
+            [dialog.utils :as utils]
+            [dialog.test.utilities :as tutils]))
 
-(deftest test-map->nsmap
+(deftest map->nsmap-test
   (let [simple-map {:a 1}
         prefixed-members {:hullo/sir 1 :foo/bar 2 :peasant 3}
         no-keywords-here {"string-key" "hi" 2 "there General Kenobi"}
@@ -13,3 +14,17 @@
            (utils/map->nsmap prefixed-members :prefix)))
     (is (= no-keywords-here (utils/map->nsmap no-keywords-here :prefix)))
     (is (= {:prefix/key1 :key2} (utils/map->nsmap keys-as-value :prefix)))))
+
+(deftest ident-map->value-test
+  (testing "No db/ident keys should be present anymore if the keys are correctly chosen."
+    (are [x y] (= x y)
+               {:foo :bar}
+               (utils/ident-map->value {:foo {:db/ident :bar}} [:foo])
+               {:foo :bar, :baz :oof}
+               (utils/ident-map->value {:foo {:db/ident :bar}, :baz {:db/ident :oof}} [:foo :baz])))
+  (testing "If the keys could not be found, there should still be the original map."
+    (are [x y] (= x y)
+               {:foo #:db{:ident :bar}}
+               (utils/ident-map->value {:foo {:db/ident :bar}} [:non-existent])))
+  (testing "Generative tests."
+    (is (tutils/check? `utils/ident-map->value))))
