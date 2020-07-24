da7827423b95b471ccaa318c2824e4f86c6d7f36
Alexander Schneider <alexander@schneider.gg>
2020-07-24 21:57:49 +0200

    Do not use db ids in tests

--- test/dialog/discussion/database_test.clj
+++ test/dialog/discussion/database_test.clj
@@ -19,25 +19,25 @@
 
 (deftest arguments-with-premise-content-test
   (testing "Should find all arguments containing a premise with certain content."
-    (let [test-result (database/arguments-with-premise-content "dogs can act as watchdogs")]
+    (let [test-result (database/arguments-with-premise-content "dogs can act as watchdogs")
+          presumed-result {:argument/version 1,
+                           :argument/author #:author{:nickname "Wegi"},
+                           :argument/type #:db{:ident :argument.type/support},
+                           :argument/premises [{:statement/content "dogs can act as watchdogs",
+                                                :statement/version 1,
+                                                :statement/author #:author{:nickname "Wegi"}}],
+                           :argument/conclusion {:statement/content "we should get a dog",
+                                                 :statement/version 1,
+                                                 :statement/author #:author{:nickname "Wegi"}}}]
       (is (= 1 (count test-result)))
       (is (= 1 (count (first test-result))))
-      (is (= {:db/id 92358976733278,
-              :argument/version 1,
-              :argument/author #:author{:nickname "Wegi"},
-              :argument/type #:db{:ident :argument.type/support},
-              :argument/premises [{:db/id 92358976733279,
-                                   :statement/content "dogs can act as watchdogs",
-                                   :statement/version 1,
-                                   :statement/author #:author{:nickname "Wegi"}}],
-              :argument/conclusion {:db/id 92358976733275,
-                                    :statement/content "we should get a dog",
-                                    :statement/version 1,
-                                    :statement/author #:author{:nickname "Wegi"}}}
-             (ffirst test-result))))))
+      (is (= (-> presumed-result :argument/premises first :statement/content)
+             (-> (ffirst test-result) :argument/premises first :statement/content)))
+      (is (= (-> presumed-result :argument/conclusion :statement/content)
+             (-> (ffirst test-result) :argument/conclusion :statement/content))))))
 
 (deftest statements-undercutting-premise-test
-  (testing "Given a original-premise deliver all premises undeructting arguments that have original-premise
+  (testing "Given a original-premise deliver all premises underucutting arguments that have original-premise
   as a premise"
     (let [desired-result (:argument/premises
                            (ffirst
